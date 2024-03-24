da9a992dbe96f56155b3fa65439c7a876d20169f
lizzy-km <oxygenisme.02@gmail.com>
2024-03-24 17:32:59 +0630

    Create POst UI Updated

--- src/Components/CreatePostBox.jsx
+++ src/Components/CreatePostBox.jsx
@@ -45,13 +45,29 @@ const CreatePostBox = () => {
     );
   }, []);
 
+  const [detail, setDetail] = useState("");
+  const [inputHeight, setInputHeight] = useState(50);
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      event.preventDefault();
+      setDetail(detail + " " + " \n ");
+      // Check for Enter key
+      //   setInputHeight(inputHeight + 50);
+    }
+    if (e.keyCode === 8 && inputHeight > 50) {
+      // Check for Enter key
+      //   setInputHeight(inputHeight / 2);
+    }
+  };
+
   return (
     <div
       id="Create_post"
       style={{
         visibility: blur === true ? "visible" : "hidden",
       }}
-      className=" text-[#d4d4d4] p-2 Create_post flex flex-col justify-between items-center rounded-md bg-[#18191a] w-[40%] h-[40%] "
+      className=" text-[#d4d4d4] p-2 Create_post flex flex-col justify-between items-center rounded-md bg-[#18191a] w-[30%] h-[35%] overflow-y-auto max-h-[35%] "
     >
       <div
         style={{
@@ -60,8 +76,8 @@ const CreatePostBox = () => {
         }}
         className=" flex flex-col justify-start items-start "
       >
-        <div className=" flex w-full h-auto justify-between items-start ">
-          <div className=" w-[80%] h-[140px] p-2  flex flex-col justify-start items-center  rounded-md ">
+        <div className=" flex w-full h-[20%] justify-between items-start ">
+          <div className=" w-[80%] h-auto p-2  flex flex-col justify-start items-center  rounded-md ">
             <div className=" w-full h-[50px] flex justify-start items-start  ">
               <div className=" cursor-pointer relative flex w-[50px]  h-[100%] justify-center items-center  rounded-full ">
                 <img
@@ -138,6 +154,68 @@ const CreatePostBox = () => {
           >
             <div className=" absolute rotate-45  w-[2px] h-[45%] rounded-full bg-[#e4e6ebb5] "></div>
             <div className=" absolute rotate-45  w-[45%] h-[2px] rounded-full bg-[#e4e6ebb5] "></div>
+          </div>
+        </div>
+        <div className=" relative flex h-[75%] overflow-y-auto max-h-[75%] flex-col outline-none p-0 justify-between items-center w-full ">
+          <div className=" relative h-auto flex flex-col gap-0 justify-start items-start w-full ">
+            <div
+              id="myInput"
+              onKeyDown={(e) => {
+                setDetail(e.currentTarget.innerText);
+              }}
+              data-lexical-editor="true"
+              aria-valuetext={detail}
+              aria-placeholder="What's on your mind, Kaung?"
+              aria-label="What's on your mind, Kaung?"
+              contentEditable="true"
+              role="textbox"
+              spellCheck="true"
+              tabIndex="0"
+              className=" relative outline-none p-2 w-full h-auto min-h-[50px] cursor-text text-[24px] break-words whitespace-pre-wrap user-select-[text]  "
+            >
+              {detail?.length < 1 && (
+                <div className=" absolute cursor-not-allowed opacity-55 user-select-none pointer-events-none ">
+                  What's on your mind, Kaung?
+                </div>
+              )}
+
+              
+            </div>
+          </div>
+
+          <div className=" self-end h-[75%] flex w-full bg-[#111111]  rounded  ">
+            <div className="flex items-center justify-center w-full">
+              <label
+                for="dropzone-file"
+                className="flex flex-col items-center justify-center w-[80%] h-[80%] border-2 border-[#343536] border-dashed rounded-lg cursor-pointer bg-[#212121] "
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    Photo / Video{" "}
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
           </div>
         </div>
       </div>
