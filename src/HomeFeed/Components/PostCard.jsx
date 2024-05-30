6f09f16ee3d532c4c55c162adfbd51119ccf252d
lizzy-km <oxygenisme.02@gmail.com>
2024-05-30 17:39:15 +0630

    Add Some Icon in Post

--- src/HomeFeed/Components/PostCard.jsx
+++ src/HomeFeed/Components/PostCard.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { storage } from "../../firebase/firebase";
 import { NavLink } from "react-router-dom";
+import Icon from "@mdi/react";
+import { mdiBookmarkOutline, mdiChatOutline, mdiHeartOutline, mdiShareOutline } from "@mdi/js";
 const PostCard = ({ name, data }) => {
   const [userProfile, setUserProfile] = useState();
   const [storyImgs, setStoryImgs] = useState();
@@ -87,24 +89,24 @@ const PostCard = ({ name, data }) => {
 
   if (hasPostD)
     return (
-      <section className=" flex flex-col p-2 gap-2 rounded-md bg-[#242526] w-full ">
-        <div className=" w-full h-[50px] p-1 flex justify-between items-center ">
-          <div className=" w-auto h-fulll flex justify-start items-start ">
-            <div className=" rounded-full  bg-[#333333] ">
+      <section className=" flex flex-col py-2  gap-2 rounded-md bg-[#242526] w-full ">
+        <div className=" w-full h-[50px] p-3 flex justify-between items-center ">
+          <div className=" w-auto h-fulll py-2 flex justify-start items-start ">
+            <NavLink to={`/${UID}`} className=" rounded-full  bg-[#333333] ">
               <img
                 className=" w-[40px] p-1 h-[40px] rounded-full object-cover cursor-pointer "
                 src={userProfile?.length > 0 ? userProfile : userAvatar}
                 alt=""
                 srcset=""
               />
-            </div>
+            </NavLink>
             <NavLink to={`/${UID}`} className=" cursor-pointer px-2 py-1 w-auto justify-start items-start text-sm h-full ">
               <p>{name}</p>
             </NavLink>
           </div>
         </div>
 
-        <p className=" p-1 ">
+        <p className=" p-3 ">
           {
             data?.post.arrayValue.values[0]?.mapValue.fields.caption
               ?.stringValue
@@ -112,6 +114,33 @@ const PostCard = ({ name, data }) => {
         </p>
 
         <img className=" w-full " src={postUrl} alt="" srcset="" />
+
+        <div className=" flex justify-between items-center py-2 px-3 " >
+            <div className=" flex gap-3 items-center  " >
+            <div className=" flex p-1 items-center cursor-pointer rounded-full " >
+            <Icon path={mdiHeartOutline} size={1} />
+
+            </div>
+
+            <div className=" flex p-1 items-center cursor-pointer rounded-full " >
+            <Icon path={mdiChatOutline} size={1} />
+
+            </div>
+
+            <div className=" flex p-1 items-center cursor-pointer rounded-full " >
+            <Icon path={mdiShareOutline} size={1} />
+
+            </div>
+
+            </div>
+          
+
+            <div className=" flex p-1 items-center cursor-pointer rounded-full " >
+            <Icon path={mdiBookmarkOutline} size={1} />
+
+            </div>
+
+        </div>
       </section>
     );
 };
