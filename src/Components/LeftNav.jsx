67f908f05ee5ae0f69b36b1fc87e6736482626ae
lizzy-km <oxygenisme.02@gmail.com>
2024-11-30 17:42:31 +0630

    Add Search By exact name

--- src/Components/LeftNav.jsx
+++ src/Components/LeftNav.jsx
@@ -1,38 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import {  setIsSearch, setUpdateFeed } from "../redux/services/authSlice";
-import { auth } from "../firebase/firebase";
+import { setIsSearch, setUpdateFeed } from "../redux/services/authSlice";
+import { app, auth, db, firestore } from "../firebase/firebase";
 import Icon from "@mdi/react";
-import {  mdiCloseCircleMultiple } from "@mdi/js";
+import { mdiCloseCircleMultiple } from "@mdi/js";
+import {
+  collection,
+  collectionGroup,
+  CollectionReference,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  queryEqual,
+  where,
+} from "firebase/firestore";
 
 const LeftNav = () => {
-  const { isTablet, isMobile } = useSelector(
-    (state) => state.animateSlice
+  const { isTablet, isMobile } = useSelector((state) => state.animateSlice);
+  const { updateFeed, isSearch } = useSelector(
+    (deserializedState) => deserializedState.authSlice
   );
-  const { updateFeed, isSearch } = useSelector((deserializedState) => deserializedState.authSlice)
 
-  const dispatch = useDispatch()
-  const user = auth?.currentUser?.uid
+  const dispatch = useDispatch();
+  const user = auth?.currentUser?.uid;
 
-  console.log(isSearch)
- useEffect(()=> { 
-  user && localStorage.setItem("adminId", user);
+  useEffect(() => {
+    user && localStorage.setItem("adminId", user);
+  }, []);
 
- },[])
-  return (
-    <section style={{
-      width:isMobile ? '100%' : isTablet ? '30%' :'30%',
-      justifyContent:isMobile ? 'end' :'start',
-      padding:isMobile ? '8px' :'0'
-    }} className= {` ${isMobile && 'flex-row-reverse'} flex w-[30%]  gap-2 items-center left_nav h-full`} >
-      <NavLink onDoubleClick={()=> {
-        // window.location.reload(true)
-        dispatch(setUpdateFeed(!updateFeed))
+  const [searchValue, setValue] = useState("");
 
+  const getSearchData = async (e) => {
+    e.preventDefault();
+    await getDocs(query(collection(firestore,"users"),where("user_name","==",e.target.value))).then((data)=>{
+     
+      setValue(data?.docs)
+      
+ } )
         
+  };
 
-      } }
+  console.log(searchValue);
+  
+
+  return (
+    <section
+      style={{
+        width: isMobile ? "100%" : isTablet ? "30%" : "30%",
+        justifyContent: isMobile ? "end" : "start",
+        padding: isMobile ? "8px" : "0",
+      }}
+      className={` ${
+        isMobile && "flex-row-reverse"
+      } flex w-[30%]  gap-2 items-center left_nav h-full`}
+    >
+      <NavLink
+        onDoubleClick={() => {
+          // window.location.reload(true)
+          dispatch(setUpdateFeed(!updateFeed));
+        }}
         to={"/"}
         className=" cursor-pointer flex rounded-full justify-center items-center "
       >
@@ -43,24 +71,71 @@ const LeftNav = () => {
         />
       </NavLink>
 
-      <div  style={{
-        width: isSearch ? '80%' : '40px',
-        justifyContent: isSearch ?  'space-between' : 'start'
-      }} className=" transition-all bg-[#2d2d2d] outline-1 outline- px-1 flex h-[40px] w-[40px] rounded-full justify-start items-center ">
-        <img onClick={()=> dispatch(setIsSearch(true)) }
+      <div
+        style={{
+          width: isSearch ? "80%" : "40px",
+          justifyContent: isSearch ? "space-between" : "start",
+        }}
+        className=" pr-2 relative transition-all bg-[#2d2d2d] outline-1 outline- px-1 flex h-[40px] w-[40px] rounded-full justify-start items-center "
+      >
+        <img
+          onClick={() => dispatch(setIsSearch(true))}
           className=" cursor-pointer px-2 "
           src="https://firebasestorage.googleapis.com/v0/b/look-vince.appspot.com/o/assets%2FSearch.svg?alt=media&token=0cc98ba7-3d8f-4f48-ad1c-5aa326bd3201"
           alt=""
         />
-        <input style={{
-          width: isSearch ? '80%' : '40px',
-            visibility: isSearch ? "visible" :'hidden'
+        <input
+          onChange={(e) => getSearchData(e)}
+          placeholder="Search"
+          style={{
+            width: isSearch ? "80%" : "40px",
+            visibility: isSearch ? "visible" : "hidden",
           }}
-          className=" transition-all bg-[#2d2d2d] outline-none border-none tracking-wide --tw-ring-color-none   px-1 flex h-[40px] w-[40px]  justify-start items-center "           type="text" name="" id="" />
-          <Icon style={{
-            visibility: isSearch ? "visible" :'hidden'
-          }} onClick={()=> dispatch(setIsSearch(false)) }  className=" cursor-pointer opacity-50  " path={mdiCloseCircleMultiple} size={1} />
-       
+          className=" transition-all bg-[#2d2d2d] outline-none border-none tracking-wide --tw-ring-color-none   px-1 flex h-[40px] w-[40px]  justify-start items-center "
+          type="text"
+          name=""
+          id=""
+        />
+        <Icon
+          style={{
+            visibility: isSearch ? "visible" : "hidden",
+          }}
+          onClick={() => dispatch(setIsSearch(false))}
+          className=" cursor-pointer opacity-50  "
+          path={mdiCloseCircleMultiple}
+          size={1}
+
+         
+        />
+         <div  style={{
+            width: isSearch ? "90%" : "40px",
+            visibility: isSearch ? "visible" : "hidden",
+          }} className=" flex justify-start items-center flex-col px-2 py-3 absolute top-[120%] left-0 w-full min-h-[200px] bg-[#2d2d2d] rounded-xl   " >
+              {
+                searchValue?.length > 0 ? searchValue?.map((sv)=> {
+                  return <NavLink to={`/${sv?._document?.data.value.mapValue.fields.UID?.stringValue}`} className=" flex text-lg justify-start items-center p-2 rounded-lg bg-[#333333] gap-2      w-[95%] h-[50px] " >
+                    
+                    <div className=" bg-[#2d2d2d] p-1 w-[50px] h-[50px] rounded-full " >
+                      <img className=" w-[100%] h-[100%] object-cover rounded-full " src={
+                      sv?._document?.data.value.mapValue.fields.profile.arrayValue.values[0].mapValue.fields.PFPATH.stringValue
+                    } alt="" srcset="" />
+                    </div>
+                          <p>{sv?._document?.data.value.mapValue.fields.user_name?.stringValue}</p>
+                  </NavLink>
+                } 
+                  
+                  
+                ) : (
+                  <div className=" flex p-2 rounded-lg bg-[#333333] w-[95%] h-[40px] " >
+                      <h1>
+                        No search data.
+                      </h1>
+                  </div>
+                )
+              }
+           
+            
+            </div>
       </div>
     </section>
   );
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { Firestore, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
+import {Database, getDatabase} from "firebase/database"
 import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
 
+const WebApp = {
+  "name": "projects/look-vince/webApps/1:1001354898081:web:ad7b211141974cf82fdee0",
+  "appId": "1:1001354898081:web:ad7b211141974cf82fdee0",
+  "displayName": "simple-facebook",
+  "projectId": "look-vince",
+  "webId": "ODZhYWE1ZGEtYjA2Zi00MjNiLWI2MzQtZmVkY2JmZTFiODky",
+  "apiKeyId": "0e6e6825-eb42-4b28-8424-10cd7e4b8027",
+  "state": "ACTIVE",
+  "expireTime": "1970-01-01T00:00:00Z",
+  "etag": "1_0253c75a-2ca0-4f45-a99d-ffa1aa2e2c62"
+}
 
+const WebConfig = {
+  "projectId": "look-vince",
+  "appId": "1:1001354898081:web:ad7b211141974cf82fdee0",
+  "databaseURL": "https://look-vince-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "look-vince.appspot.com",
+  "locationId": "asia-southeast1",
+  "apiKey": "AIzaSyBIUKGA_wTZ57l2mjLpKVgD3WyQT6XCBXs",
+  "authDomain": "look-vince.firebaseapp.com",
+  "messagingSenderId": "1001354898081",
+  "measurementId": "G-0Y8J2Q1G13"
+}
 
 
 const firebaseConfig = {
@@ -21,8 +44,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
-
+firebase?.initializeApp(firebaseConfig)
 // Initialize Firebase Authentication and get a reference to the service
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,5 +53,6 @@ const auth = getAuth(app);
 
 const storage = getStorage(app);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export { app, auth, firestore, storage, analytics };
+export { app, auth, firestore, storage, analytics,db };
